6bd1078d146dda3ec33f95eeeca1c43d719ebb1f
SLH <salihmca@gmail.com>
2025-08-20 10:07:49 +0300

    initial commit

---
+++ assets/js/login.js
@@ -0,0 +1,16 @@
+// Login page
+
+$(document).ready(function () {
+  // Login page
+  $(".auth-option-container").on("click", function () {
+    $(".login-option").toggle();
+    $(".login-paci-form").toggle();
+  });
+
+  $("#btn-login-back").on("click", function () {
+    $(".login-option").toggle();
+    $(".login-paci-form").toggle();
+  });
+
+  back-login-option
+});
